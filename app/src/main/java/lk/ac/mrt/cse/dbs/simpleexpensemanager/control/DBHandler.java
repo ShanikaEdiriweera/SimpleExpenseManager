0360649ff1c82778d947eee9e075b2241fe83465
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-12-06 18:24:37 +0530

    DBHandler added

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/DBHandler.java
@@ -0,0 +1,46 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.Constants;
+
+/**
+ * Created by SHAEDI on 06/12/2015.
+ */
+public class DBHandler extends SQLiteOpenHelper{
+    private static DBHandler dbHandler = null;
+
+    private DBHandler(Context context) {
+        super(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_ACCOUNT_TABLE = "CREATE TABLE " + Constants.TABLE_ACCOUNT + "( Account_No VARCHAR(10) PRIMARY KEY, Account_holder_name VARCHAR(50) NOT NULL," +
+                "Bank_name VARCHAR(30) NOT NULL, Balance double default 0 check(Balance>=0) )";
+
+        String CREATE_TRANSACTION_TABLE = "CREATE TABLE " + Constants.TABLE_TRANSACTION + "( Date DATETIME default '0000-00-00 00:00:00'" +
+                "Account_No VARCHAR(10) NOT NULL, Expense_type VARCHAR(7) check(Expense_type in ('EXPENSE','INCOME')), Amount double default 0 check(balance>=0)," +
+                "primary key(Date, Account_No)), foreign key(Account_No) references "+Constants.TABLE_ACCOUNT+"(Account_No)";
+
+        db.execSQL(CREATE_ACCOUNT_TABLE);
+        db.execSQL(CREATE_TRANSACTION_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_ACCOUNT);
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_TRANSACTION);
+
+        // Create tables again
+        onCreate(db);
+    }
+}
+
+
+
