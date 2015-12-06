3766790db39a22258163e37aa422486fe2bcb5dc
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-12-06 21:33:10 +0530

    Code done

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -0,0 +1,74 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.Constants;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
+/**
+ * Created by SHAEDI on 06/12/2015.
+ */
+public class PersistentTransactionDAO implements TransactionDAO {
+    private SQLiteDatabase db = null;
+
+    public PersistentTransactionDAO(SQLiteDatabase db) {
+        this.db = db;
+    }
+    @Override
+    public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        db.execSQL("INSERT INTO "+ Constants.TABLE_TRANSACTION+" values("+formatter.format(date)+","+accountNo+","+expenseType+","+amount+");");
+    }
+
+    @Override
+    public List<Transaction> getAllTransactionLogs() {
+        Cursor cursor = db.rawQuery("Select * from Transaction",null);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        if (cursor.moveToFirst()) {
+            do {
+                try {
+                    Date date = formatter.parse(cursor.getString(0));
+                    Transaction transaction = new Transaction(date,cursor.getString(1),((cursor.getString(2)=="INCOME")?ExpenseType.INCOME:ExpenseType.EXPENSE), Double.parseDouble(cursor.getString(3)));
+                    // Adding contact to list
+                    transactions.add(transaction);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            } while (cursor.moveToNext());
+        }
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        Cursor cursor = db.rawQuery("Select * from Transaction ORDER BY Date LIMIT "+limit,null);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        if (cursor.moveToFirst()) {
+            do {
+                try {
+                    Date date = formatter.parse(cursor.getString(0));
+                    Transaction transaction = new Transaction(date,cursor.getString(1),((cursor.getString(2)=="INCOME")?ExpenseType.INCOME:ExpenseType.EXPENSE), Double.parseDouble(cursor.getString(3)));
+                    // Adding contact to list
+                    transactions.add(transaction);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            } while (cursor.moveToNext());
+        }
+        return transactions;
+    }
+}
